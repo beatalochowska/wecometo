f549c52903a861b54459a6f10439fcb8e0f8bb62
Beata Lochowska <Beata.Lochowska@emerson.com>
2021-05-02 16:19:38 +0200

    adding colors to cards

--- src/components/common/cards/featureCard/FeatureCard.tsx
+++ src/components/common/cards/featureCard/FeatureCard.tsx
@@ -1,11 +1,34 @@
 import styles from "../Card.module.scss";
+import * as feature from "../../../../constants/features";
 
 interface FeatureCardProps {
   title: string;
 }
+
+const createCardStyle = (name: string, elementStyle: string): string => {
+  const style = elementStyle;
+  switch (name) {
+    case feature.STOCK:
+      return `${style} ${styles.stock}`;
+    case feature.WORK:
+      return `${style} ${styles.work}`;
+    case feature.FENCE:
+      return `${style} ${styles.fence}`;
+    case feature.PARK:
+      return `${style} ${styles.park}`;
+    case feature.POOL:
+      return `${style} ${styles.pool}`;
+    case feature.BIS:
+      return `${style} ${styles.bis}`;
+    default:
+      return style;
+  }
+};
 export default function FeatureCard(props: FeatureCardProps): JSX.Element {
+  console.log(props.title);
+
   return (
-    <div className={styles.card}>
+    <div className={createCardStyle(props.title, styles.card)}>
       <h2 className={styles.cardContent}>{props.title}</h2>
     </div>
   );
