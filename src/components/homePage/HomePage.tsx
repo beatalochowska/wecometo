0ec27fc21d34a434b4d2c37a5a6b5f7652718650
Beata Lochowska <Beata.Lochowska@emerson.com>
2021-05-08 22:02:24 +0200

    creating randomise lists once functions

--- src/components/homePage/HomePage.tsx
+++ src/components/homePage/HomePage.tsx
@@ -1,60 +1,40 @@
 import { useEffect, useState } from "react";
 import CardsSet from "../common/cardsSet/CardsSet";
 import styles from "./HomePage.module.scss";
-import { getRandomRate, getCurrentElement } from "./helpers";
+import {
+  getRandomisedList,
+  getListOFNumbersFeaturesWithSortedIds,
+} from "./helpers";
 
 import features from "../../tools/features.json";
 import { SingleFeature } from "../../tools/featureInterface";
 import numbers from "../../tools/numbers.json";
 import { SigleNumber } from "../../tools/numberInterface";
+import { CARDS_AMOUNT } from "../../constants/cardsAmount";
+
+const randomisedFeaturesList = getRandomisedList([...features]);
+const randomisedNumbersList = getRandomisedList([...numbers]);
 
 export default function HomePage(): JSX.Element {
-  const [featuresList, setFeaturesList] = useState<SingleFeature[]>([
-    ...features,
-  ]);
-  const [numbersList, setNumbersList] = useState<SigleNumber[]>([...numbers]);
   const [currentCardIndex, setCurrentCardIndex] = useState<number>(0);
-  const [randList, setRandList] = useState([]);
 
   const [currentCardValues, setCurrentCardValues] = useState<
     (SigleNumber & SingleFeature)[]
   >([]);
 
-  const getRandomisedList = (arr: any[]): any[] => {
-    return arr.sort(() => getRandomRate(arr));
-  };
-
-  const getListOFNumbersFeaturesWithSortedIds = (
-    randomisedFeatures: SingleFeature[],
-    randomisedNumbers: SigleNumber[]
-  ): (SigleNumber & SingleFeature)[] => {
-    const featuresAndNumbersList = randomisedNumbers.map(
-      (el: SigleNumber, i: number) =>
-        Object.assign({}, el, randomisedFeatures[i], { id: i })
-    );
-
-    return featuresAndNumbersList;
-  };
-
   const setCurrentCardsSet = (
-    cardsAmount: number,
     cardsList: (SigleNumber & SingleFeature)[]
   ): (SigleNumber & SingleFeature)[] => {
     const currentCards = [];
     let index = currentCardIndex;
-    for (let i = index; i < index + cardsAmount; i++) {
+    for (let i = index; i < index + CARDS_AMOUNT; i++) {
       currentCards.push(cardsList[i]);
     }
-    index += cardsAmount;
+    index += CARDS_AMOUNT;
     setCurrentCardIndex(index);
-
-    console.log(currentCards);
-    console.log(currentCardIndex);
 
     return currentCards;
   };
-  const randomisedFeaturesList = getRandomisedList(featuresList);
-  const randomisedNumbersList = getRandomisedList(numbersList);
 
   const randomisedList = getListOFNumbersFeaturesWithSortedIds(
     randomisedFeaturesList,
@@ -62,13 +42,11 @@ export default function HomePage(): JSX.Element {
   );
 
   useEffect((): void => {
-    console.log(randomisedList);
-    setCurrentCardValues(setCurrentCardsSet(3, randomisedList));
+    setCurrentCardValues(setCurrentCardsSet(randomisedList));
   }, []);
 
   const handleNextClick = (): void => {
-    console.log(randomisedList);
-    setCurrentCardValues(setCurrentCardsSet(3, randomisedList));
+    setCurrentCardValues(setCurrentCardsSet(randomisedList));
   };
 
   return (
--- src/components/homePage/helpers.ts
+++ src/components/homePage/helpers.ts
@@ -1,24 +1,27 @@
 import seedrandom from "seedrandom";
+import { SingleFeature } from "../../tools/featureInterface";
+import { SigleNumber } from "../../tools/numberInterface";
 
 let seed = "czeko1adaje5tsuper!";
+export const getRandomRate = (): number => {
+  const randomRate = seedrandom(seed)() - 0.5;
+  seed = seed + "lala";
 
-export const getRandomRate = <T>(arr: T[]): number => {
-  seed = seed + "6*toff1te7";
-  seed = seed.substring(1);
-  const randomIndex = seedrandom(seed)() - 0.5;
-
-  return randomIndex;
+  return randomRate;
 };
 
-export const getArrayWithDeletedCurrentElement = <T>(
-  arr: T[],
-  index: number
-): void => {
-  arr.splice(index, 1);
+export const getRandomisedList = (arr: any[]): any[] => {
+  return arr.sort(() => getRandomRate());
 };
 
-export const getCurrentElement = <T>(arr: T[], index: number): T => {
-  const currentElement = arr[index];
+export const getListOFNumbersFeaturesWithSortedIds = (
+  randomisedFeatures: SingleFeature[],
+  randomisedNumbers: SigleNumber[]
+): (SigleNumber & SingleFeature)[] => {
+  const featuresAndNumbersList = randomisedNumbers.map(
+    (el: SigleNumber, i: number) =>
+      Object.assign({}, el, randomisedFeatures[i], { id: i })
+  );
 
-  return currentElement;
+  return featuresAndNumbersList;
 };
