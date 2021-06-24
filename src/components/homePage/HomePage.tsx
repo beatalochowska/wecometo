34ea964b4ce9fe8be8f9b48cf71a69190d1a1ab2
Beata Lochowska <Beata.Lochowska@emerson.com>
2021-06-24 19:36:33 +0200

    fixing displaying correct first 3 cards

--- src/components/homePage/HomePage.tsx
+++ src/components/homePage/HomePage.tsx
@@ -1,26 +1,57 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import CardsSet from "../common/cardsSet/CardsSet";
 import styles from "./HomePage.module.scss";
-import {
-  getRandomisedList,
-  getListOFNumbersFeaturesWithSortedIds,
-} from "./helpers";
+import { getListOFNumbersFeaturesWithSortedIds } from "./helpers";
 
 import features from "../../tools/features.json";
 import { SingleFeature } from "../../tools/featureInterface";
 import numbers from "../../tools/numbers.json";
 import { SigleNumber } from "../../tools/numberInterface";
 import { CARDS_AMOUNT } from "../../constants/cardsAmount";
+import Chance from "chance";
 
-const randomisedFeaturesList = getRandomisedList([...features]);
-const randomisedNumbersList = getRandomisedList([...numbers]);
+const useDidMountEffect = (func: any, deps: any) => {
+  const didMount = useRef(false);
+
+  useEffect(() => {
+    if (didMount.current) {
+      func();
+    } else {
+      didMount.current = true;
+    }
+  }, deps);
+};
 
 export default function HomePage(): JSX.Element {
   const [currentCardIndex, setCurrentCardIndex] = useState<number>(0);
-
   const [currentCardValues, setCurrentCardValues] = useState<
     (SigleNumber & SingleFeature)[]
   >([]);
+
+  const [userSeed, setUserSeed] = useState<string>("");
+
+  const [shouldShowCards, setShouldRandomiseCards] = useState<boolean>(false);
+
+  const getRandomisedList = (arr: any[], userSeed: string): any[] => {
+    const randomRate = new Chance(userSeed);
+
+    return randomRate.shuffle(arr);
+  };
+
+  const randomisedFeaturesList = () =>
+    getRandomisedList([...features], userSeed);
+  const randomisedNumbersList = () => getRandomisedList([...numbers], userSeed);
+
+  const [randomisedList, setRandomisedList] = useState<
+    (SigleNumber & SingleFeature)[]
+  >(
+    getListOFNumbersFeaturesWithSortedIds(
+      randomisedFeaturesList(),
+      randomisedNumbersList()
+    )
+  );
+
+  const allCardsAmount = randomisedList.length;
 
   const setCurrentCardsSet = (
     cardsList: (SigleNumber & SingleFeature)[]
@@ -31,31 +62,62 @@ export default function HomePage(): JSX.Element {
       currentCards.push(cardsList[i]);
     }
     index += CARDS_AMOUNT;
-    setCurrentCardIndex(index);
+    if (index < allCardsAmount) {
+      setCurrentCardIndex(index);
+    } else {
+      setCurrentCardIndex(0);
+      setUserSeed(userSeed + "la123");
+      setRandomisedList(
+        getListOFNumbersFeaturesWithSortedIds(
+          randomisedFeaturesList(),
+          randomisedNumbersList()
+        )
+      );
+    }
 
     return currentCards;
   };
 
-  const randomisedList = getListOFNumbersFeaturesWithSortedIds(
-    randomisedFeaturesList,
-    randomisedNumbersList
-  );
-
-  useEffect((): void => {
+  useDidMountEffect(() => {
     setCurrentCardValues(setCurrentCardsSet(randomisedList));
-  }, []);
+  }, [shouldShowCards]);
 
   const handleNextClick = (): void => {
     setCurrentCardValues(setCurrentCardsSet(randomisedList));
   };
 
+  const handleChangeUserSeed = (event: any): void => {
+    setUserSeed(event.target.value);
+  };
+
+  const handleSeedSubmit = (event: any): void => {
+    event.preventDefault();
+
+    setRandomisedList(
+      getListOFNumbersFeaturesWithSortedIds(
+        randomisedFeaturesList(),
+        randomisedNumbersList()
+      )
+    );
+
+    setShouldRandomiseCards(true);
+  };
+
   return (
     <section className={styles.homePage}>
       <h1 className={styles.title}>Welcome to my street</h1>
-      <CardsSet currentCards={currentCardValues} />
-      <button className={styles.button} onClick={handleNextClick}>
-        NEXT
-      </button>
+      <form onSubmit={handleSeedSubmit}>
+        <input type="text" value={userSeed} onChange={handleChangeUserSeed} />
+        <input type="submit" value="Generuj karty" />
+      </form>
+      {shouldShowCards && (
+        <>
+          <CardsSet currentCards={currentCardValues} />
+          <button className={styles.button} onClick={handleNextClick}>
+            NEXT
+          </button>
+        </>
+      )}
     </section>
   );
 }
--- src/components/homePage/helpers.ts
+++ src/components/homePage/helpers.ts
@@ -1,18 +1,5 @@
-import seedrandom from "seedrandom";
 import { SingleFeature } from "../../tools/featureInterface";
 import { SigleNumber } from "../../tools/numberInterface";
-
-let seed = "czeko1adaje5tsuper!";
-export const getRandomRate = (): number => {
-  const randomRate = seedrandom(seed)() - 0.5;
-  seed = seed + "lala";
-
-  return randomRate;
-};
-
-export const getRandomisedList = (arr: any[]): any[] => {
-  return arr.sort(() => getRandomRate());
-};
 
 export const getListOFNumbersFeaturesWithSortedIds = (
   randomisedFeatures: SingleFeature[],
